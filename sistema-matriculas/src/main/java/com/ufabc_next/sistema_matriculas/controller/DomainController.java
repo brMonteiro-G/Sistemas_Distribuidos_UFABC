e6d22f9c68ba95864a3a5ec8187722ad34899ff1
Mateus Braga <teteu040506@gmail.com>
2025-08-06 18:58:42 -0300

    chore: queues blueprint

--- sistema-matriculas/src/main/java/com/ufabc_next/sistema_matriculas/controller/DomainController.java
+++ sistema-matriculas/src/main/java/com/ufabc_next/sistema_matriculas/controller/DomainController.java
@@ -3,6 +3,7 @@ package com.ufabc_next.sistema_matriculas.controller;
 import org.springframework.web.bind.annotation.*;
 
 import static com.ufabc_next.sistema_matriculas.core.leaderElection.LeaderElection.leaderElection;
+import static com.ufabc_next.sistema_matriculas.core.queues.Queues.queueTest;
 
 
 @RestController
@@ -12,5 +13,10 @@ public class DomainController {
         @PostMapping("/elect")
         public void electLeader(@RequestBody String[] args) {
             leaderElection(args);
+        }
+
+        @PostMapping("/elect")
+        public void QueuesTest(@RequestBody String[] args) {
+            queueTest(args)
         }
 }
--- sistema-matriculas/src/main/java/com/ufabc_next/sistema_matriculas/core/queues/Queues.java
+++ sistema-matriculas/src/main/java/com/ufabc_next/sistema_matriculas/core/queues/Queues.java
@@ -1,4 +1,41 @@
 package com.ufabc_next.sistema_matriculas.core.queues;
 
-public class Queues {
+public class Queues implements Watcher {
+      public static void queueTest(String args[]) {
+        Queue q = new Queue(args[1], "/app3");
+
+        System.out.println("Input: " + args[1]);
+        int i;
+        Integer max = Integer.valueOf(args[2]);
+
+        if (args[3].equals("p")) {
+            System.out.println("Producer");
+            for (i = 0; i < max; i++)
+                try{
+                    q.produce(10 + i);
+                } catch (KeeperException e){
+                    e.printStackTrace();
+                } catch (InterruptedException e){
+			    e.printStackTrace();
+                }
+        } else {
+            System.out.println("Consumer");
+
+            for (i = 0; i < max; i++) {
+                try{
+                    int r = q.consume();
+                    System.out.println("Item: " + r);
+                } catch (KeeperException e){
+                    i--;
+                } catch (InterruptedException e){
+			    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+        @Override
+    public void process(WatchedEvent watchedEvent) {
+
+    }
 }
