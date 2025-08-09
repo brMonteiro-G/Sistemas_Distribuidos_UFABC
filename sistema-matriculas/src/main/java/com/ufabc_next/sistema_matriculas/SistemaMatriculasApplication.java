d6cf0bf8300eaf9b2e5d50cf51666ba7088095df
Igor <sigorney@ithub.com>
2025-08-09 17:54:04 -0300

    feat: start leader election when barrier reached

--- sistema-matriculas/src/main/java/com/ufabc_next/sistema_matriculas/SistemaMatriculasApplication.java
+++ sistema-matriculas/src/main/java/com/ufabc_next/sistema_matriculas/SistemaMatriculasApplication.java
@@ -1,13 +1,24 @@
 package com.ufabc_next.sistema_matriculas;
 
+import com.ufabc_next.sistema_matriculas.domain.common.Barrier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SistemaMatriculasApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SistemaMatriculasApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaMatriculasApplication.class, args);
+
+        try {
+            Barrier barrier = new Barrier("host.docker.internal:2181", // endereço do ZooKeeper
+                    "/barrier"                 // znode da barreira
+            );
+            barrier.enter();
+            System.out.println("Entered barrier: /barrier");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
--- sistema-matriculas/src/main/java/com/ufabc_next/sistema_matriculas/controller/DomainController.java
+++ sistema-matriculas/src/main/java/com/ufabc_next/sistema_matriculas/controller/DomainController.java
@@ -1,7 +1,10 @@
 package com.ufabc_next.sistema_matriculas.controller;
 
 import com.ufabc_next.sistema_matriculas.core.locks.Locks;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import static com.ufabc_next.sistema_matriculas.core.leaderElection.LeaderElection.*;
 import static com.ufabc_next.sistema_matriculas.core.queues.Queues.queueTest;
@@ -11,30 +14,30 @@ import static com.ufabc_next.sistema_matriculas.core.queues.Queues.queueTest;
 @RequestMapping("/domain")
 public class DomainController {
 
-        @PostMapping("/elect")
-        public void electLeader(@RequestBody String[] args) {
-            leaderElection(args);
-        }
+    @PostMapping("/elect")
+    public void electLeader(@RequestBody String[] args) {
+        leaderElection(args);
+    }
 
 
-        @PostMapping("/leader/produce")
-        public void produceMessagess(@RequestBody String[] args) {
-                produce(args);
-        }
+    @PostMapping("/leader/produce")
+    public void produceMessagess(@RequestBody String[] args) {
+        produce(args);
+    }
 
-        @PostMapping("/follower/consume")
-        public void consumeMessage(@RequestBody String[] args) {
-            consume(args);
-        }
+    @PostMapping("/follower/consume")
+    public void consumeMessage(@RequestBody String[] args) {
+        consume(args);
+    }
 
 
     @PostMapping("/queue")
-        public void QueuesTest(@RequestBody String[] args) {
-            queueTest(args);
-        }
+    public void QueuesTest(@RequestBody String[] args) {
+        queueTest(args);
+    }
 
-        @PostMapping("/lock")
-        public void lock(@RequestBody String[] args) {
-            Locks.exec(args);
-        }
+    @PostMapping("/lock")
+    public void lock(@RequestBody String[] args) {
+        Locks.exec(args);
+    }
 }
